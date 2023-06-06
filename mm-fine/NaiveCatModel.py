43e21c1c3bdaab9b19f55218c9f8232a17c27e84
minouei-kl <74264912+minouei-kl@users.noreply.github.com>
2023-06-06 18:51:36 +0200

    Update NaiveCatModel.py

--- mm-fine/NaiveCatModel.py
+++ mm-fine/NaiveCatModel.py
@@ -2,38 +2,36 @@ import torch
 import torch.nn as nn
 from transformers import AutoModel
 from resnet import resnet50
+import torch.nn.functional as F
 
 
 class TextModel(nn.Module):
 
     def __init__(self, config):
         super(TextModel, self).__init__()
-        bert_name = '/home/minouei/Documents/models/rvl/multimodal/bert_model'
+        bert_name = '/home/minouei/Documents/models/rvl/bert-im/bert_model4'
         # self.bert = AutoModel.from_pretrained(bert_name)
         self.bert = torch.load(bert_name)
-        # self.trans = nn.Sequential(
-        #     nn.Dropout(config.bert_dropout),
-        #     nn.Linear(self.bert.config.hidden_size, config.middle_hidden_size),
-        #     nn.ReLU(inplace=True)
-        # )
 
         # 是否进行fine-tune
-        for param in self.bert.parameters():
-            if config.fixed_text_model_params:
-                param.requires_grad = False
+        # for param in self.bert.parameters():
+        #     if config.fixed_text_model_params:
+        #         param.requires_grad = False
+        #     else:
+        #         param.requires_grad = True
+        for n, p in self.bert.named_parameters():
+            if 'classifier' in n:
+                pass
             else:
-                param.requires_grad = True
-
-        # self.bert.init_weights()
+                p.requires_grad = False
 
     def forward(self, bert_inputs, masks, token_type_ids=None):
         assert bert_inputs.shape == masks.shape, 'error! bert_inputs and masks must have same shape!'
         bert_out = self.bert(
             input_ids=bert_inputs, token_type_ids=token_type_ids, attention_mask=masks)
-        pooler_out = bert_out['logits']
+        logits = bert_out['logits']
 
-        return pooler_out
-        # return self.trans(pooler_out)
+        return logits
 
 
 class ImageModel(nn.Module):
@@ -44,27 +42,21 @@ class ImageModel(nn.Module):
         self.full_resnet = resnet50(num_classes=16, channel=3)
         checkpoint = torch.load(resume, map_location='cuda:0')
         self.full_resnet.load_state_dict(checkpoint['state_dict'])
-        # self.resnet = nn.Sequential(
-        #     *(list(self.full_resnet.children())[:-1]),
-        #     nn.Flatten()
-        # )
-        # self.trans = nn.Sequential(
-        #     nn.Dropout(config.resnet_dropout),
-        #     nn.Linear(self.full_resnet.fc.in_features,
-        #               config.middle_hidden_size),
-        #     nn.ReLU(inplace=True)
-        # )
 
         # 是否进行fine-tune
-        for param in self.full_resnet.parameters():
-            if config.fixed_image_model_params:
-                param.requires_grad = False
+        # for param in self.full_resnet.parameters():
+        #     if config.fixed_image_model_params:
+        #         param.requires_grad = False
+        #     else:
+        #         param.requires_grad = True
+        for n, p in self.full_resnet.named_parameters():
+            if 'fc' in n:
+                pass
             else:
-                param.requires_grad = True
+                p.requires_grad = False
 
     def forward(self, imgs):
         output, _ = self.full_resnet(imgs)
-        # return self.trans(feature)
         return output
 
 
@@ -79,17 +71,16 @@ class Model(nn.Module):
 
         # 全连接分类器
         self.classifier = nn.Sequential(
-            # nn.Dropout(config.fuse_dropout),
-            # nn.Linear(config.num_labels * 2, config.num_labels * 2),
-            # nn.ReLU(inplace=True),
-            # nn.Dropout(config.fuse_dropout),
-            nn.Linear(config.num_labels * 2, config.num_labels)
+            nn.BatchNorm1d(32),
+            nn.Linear(32, config.num_labels)
         )
         # self.loss_func = nn.CrossEntropyLoss()
 
     def forward(self, text, image, label=None):
         text_feature = self.text_model(text[0], text[1])
         img_feature = self.img_model(image)
+        # text_feature = F.sigmoid(text_feature)
+        # img_feature = F.sigmoid(img_feature)
         # print(text_feature)
         # print(img_feature)
         preds = self.classifier(
@@ -97,10 +88,18 @@ class Model(nn.Module):
         )
 
         return preds
-        # pred_labels = torch.argmax(prob_vec, dim=1)
 
-        # if label is not None:
-        #     loss = self.loss_func(prob_vec, label)
-        #     return pred_labels, loss
-        # else:
-        #     return pred_labels
+
+class Model2(nn.Module):
+
+    def __init__(self, config):
+        super(Model2, self).__init__()
+        self.classifier = nn.Sequential(
+
+            nn.Linear(config.num_labels * 2, config.num_labels)
+        )
+
+    def forward(self, feature, label=None):
+        preds = self.classifier(feature)
+
+        return preds
